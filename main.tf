edfc4d42f3fdf701e54ec649c3e227262cd3261e
ineso <touziines200@gmail.com>
2020-06-10 10:56:18 +0100

    IT: Update files with using the concept of env variables

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
 provider "google" {
   version = "3.5.0"
-  credentials = file("credentials.json")
-  project = "pfe-ines"
-  region  = "us-central1"
+  credentials = "${file("${var.credentials}")}"
+  project     = "${var.gcp_project}" 
+  region      = "${var.region}"
   zone    = "us-central1-c"
 }
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "${var.name}-vpc"
+  auto_create_subnetworks="false"
 }
 resource "google_compute_subnetwork" "public-subnetwork" {
-  name          = "terraform-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.vpc_network.name
+  name          = "${var.name}-subnet"
+  ip_cidr_range = "${var.subnet_cidr}"
+  region        = "${var.region}"
+  network       = "${var.name}-vpc"
+  depends_on    = ["google_compute_network.vpc_network"]
   }
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+region          = "us-central1"
+gcp_project     = "pfe-ines"
+credentials     = "credentials.json"
+name            = "rif"
+subnet_cidr     = "10.2.0.0/16"
